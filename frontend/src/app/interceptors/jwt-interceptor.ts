7ce15c256d4d7f68c39b31244e5118e203382481
Daryn Abylai <darynabylai3@gmail.com>
2026-04-19 19:05:13 +0500

    fixed some of bugs

--- frontend/src/app/interceptors/jwt-interceptor.ts
+++ frontend/src/app/interceptors/jwt-interceptor.ts
@@ -4,13 +4,10 @@ import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
   const token = localStorage.getItem('access');
 
-  // Не добавляем токен к публичным эндпоинтам
-  const publicUrls = ['/api/products/'];
-  const isPublic = publicUrls.some(url => req.url.includes(url));
-
-  if (token && !isPublic) {
+  if (token) {
     req = req.clone({
       setHeaders: { Authorization: `Bearer ${token}` }
     });
@@ -21,7 +18,7 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
       if (err.status === 401) {
         localStorage.removeItem('access');
         localStorage.removeItem('refresh');
-        inject(Router).navigate(['/login']);
+        router.navigate(['/login']);
       }
       return throwError(() => err);
     })
